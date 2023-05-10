f91ba03c008f35e0fc22a16e687448a66d4c064f
dci <youssef.alzibak@dci.education>
2023-05-10 16:39:13 +0200

    AusgelagerteVeriablen

--- WetterApp/src/components/FuenfTagesVorschau.jsx
+++ WetterApp/src/components/FuenfTagesVorschau.jsx
@@ -1,19 +1,17 @@
 import { useContext } from "react";
 import { ContextWetter } from "../context/ContextProvider";
-// import { faWind } from "@fortawesome/free-solid-svg-icons";
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {iconWetter} from '../actions/globalVariable'
 const FuenfTagesVorschau = () => {
-    const { wetterDataTag, sprache, wochenTageAr, wochenTageDe, wochenTageEn } =
+    const {iconWetter, wetterDataTag, sprache, wochenTageAr, wochenTageDe, wochenTageEn } =
         useContext(ContextWetter);
-
-  
-
+    
+    console.log(wetterDataTag);
+    
     const filteredWetterData = wetterDataTag?.list
         .slice(0, 42)
-        .filter((_, index) => index % 9 === 0);
-    // console.log(filteredWetterData);
+        .filter((_, index) => index % 8 === 0);
     const getDayOfWeek = (dateString) => {
+
+        // die Tage werden mit sprituperetur aus gebackt da die von anderen data kommen 
         const daysOfWeek =
             sprache === "en"
                 ? [...wochenTageEn]
@@ -23,9 +21,6 @@ const FuenfTagesVorschau = () => {
         const date = new Date(dateString);
         return daysOfWeek[date.getDay()];
     };
-    if (!wetterDataTag || wetterDataTag.cod === "404") {
-        return null
-    }
     return (
         <div className="fuenftageslink display">
             <div className="wetterdata-countener">
@@ -37,33 +32,23 @@ const FuenfTagesVorschau = () => {
                         <img
                             className="img_fuenf_tage"
                             src={`${iconWetter(wetterData.weather[0].icon)}`}
-                            alt=""
+                            
                         />
                         <div className="wetterdata-item">
                             <p>{wetterData.main.temp.toFixed()} °C</p>
                         </div>
                         <div className="wetterdata-item">
-                            <p>{wetterData.weather[0].description}</p>
+                            <p className="fuenf">{wetterData.weather[0].description}</p>
                         </div>
                         <div className="wetterdata-item">
-                            <p>
-                                {/* <FontAwesomeIcon icon={faWind}/> */}{" "}
+                            <p className="windspeed">
                                 {wetterData.wind.speed} km/h
                             </p>
                         </div>
                     </div>
                 ))}
             </div>
-
-            <p>
-                {/* {sprache === "de"
-                    ? "Fünf Tages Wettervorschau"
-                    : sprache === "ar"
-                    ? "توقعات الطقس لمدة خمسة أيام"
-                    : "Five day weather forecast"} */}
-            </p>
         </div>
     );
 };
-
 export default FuenfTagesVorschau;
